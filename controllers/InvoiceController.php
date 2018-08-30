c396725ba6681d9d2eed0ec1cb7047c2f36b1213
Romain Vandevoorde <romain.vandevoorde.dev@gmail.com>
2018-08-30 10:20:11 +0200

    remove legacy code

--- controllers/InvoiceController.php
+++ controllers/InvoiceController.php
@@ -1,13 +1,5 @@
 <?php
 
-function showInvoice() {
-  if(isset($_GET['id'])) {
-    $view_data['content'] = '<h3>Facture n°'.$_GET['id'].'</h3>';
-    require PATH_VIEWS."/layout.php";
-  }
-  else exit('prout');
-}
-
 class InvoiceController
 {
   public function show($id) {
--- router.php
+++ router.php
@@ -43,44 +43,5 @@ route('GET', '/^factures\/([0-9]+)\/?$/', $actual_url, 'InvoiceController', 'sho
 route('GET', '/^factures\/societe\/([0-9]+)\/?$/', $actual_url, 'InvoiceController', 'indexByCompany');
 route('GET', '/^factures\/?$/', $actual_url, 'InvoiceController', 'index');
 
-// if(preg_match('/^factures\/([0-9]+)\/?$/', $actual_url, $args)) {
-//   echo '-> Afficher facture N°'.$args[1];
-//   exit;
-// }
-// if(preg_match('/^factures\/societe\/([0-9]+)\/?$/', $actual_url, $args)) {
-//   echo '-> Afficher les factures de la société n°'.$args[1];
-//   exit;
-// }
-// if(preg_match('/^factures\/?$/', $actual_url, $args)) {
-//   echo '-> Afficher toutes les factures';
-//   exit;
-// }
-
-// if(isset($_GET['action'])) $action = $_GET['action'];
-// else $action = "home";
-//
-// echo '<p>Action: '.$action.'</p>';
-//
-// switch($action) {
-//   case "home":
-//     guard(1);
-//     require PATH_CONTROLLERS."/LoginController.php";
-//     showForm();
-//     break;
-//   case "login":
-//     require PATH_CONTROLLERS."/LoginController.php";
-//     if($method === "GET") showForm();
-//     elseif($method === "POST") processLogin();
-//     else redirect('home');
-//     break;
-//   case "showinvoice":
-//     require PATH_CONTROLLERS."/InvoiceController.php";
-//     showInvoice();
-//     break;
-//   default:
-//     require PATH_CONTROLLERS."/LoginController.php";
-//     showForm();
-//     break;
-// }
 
 ?>

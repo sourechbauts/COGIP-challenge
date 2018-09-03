898a1c0def66c1afed38385d8be4f764dd81795f
Romain Vandevoorde <romain.vandevoorde.dev@gmail.com>
2018-09-03 09:43:57 +0200

    login controller sample use

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -8,7 +8,9 @@ class LoginController
 
   public function login() {
     if(!post_isset(['login_user', 'login_pwd'])) redirect('login');
-    $content = '<h2>process login</h2>';
+    $content = '<h2>Process Login</h2>';
+    $content .= '<p>User: '.$_POST['login_user'].'</p>';
+    $content .= '<p>Pass: '.$_POST['login_pwd'].'</p>';
     view('layout', array('content' => $content));
   }
 }
